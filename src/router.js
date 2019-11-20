e13c475429ca36a4934cf3f43b919b5c5ac0be63
Roshan Thaabith <roshlarosh@gmail.com>
2019-11-20 15:59:28 +0000

    create handleAll function

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-const handler = require('./handlers.js');
+const handlers = require('./handlers.js');
 // const fs = require('fs');
 // const path = require('path');
 
@@ -6,7 +6,15 @@ const router = (request, response) => {
     const endpoint = request.url;
     console.log(endpoint);
     // IF ELSE STATEMENT GOES HERE
+    if(endpoint === '/') {
+        handlers.handleHome(request, response);
+    }else if (endpoint.indexOf('public') !== -1){
+        handlers.handleAll(request, response);
+    }else {
+        response.writeHead(404);
+        response.end('404 not found');
+    }
 }
 
 
-module.exports = handler;
+module.exports = router;
