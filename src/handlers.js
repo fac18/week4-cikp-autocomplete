8bbcf0f6e6e58140a84d68cdbfa4e6055da906ed
Kin Au <kinnamau@gmail.com>
2019-11-20 14:39:54 +0000

    created server
    Co-authored-by: roshan <roshlarosh@gmail.com>

---
+++ src/handlers.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const querystring = require('querystring');
+const path = require('path');
+
+
+const handleHome = (request, response) => {
+    const filePath = path.join(__dirname, '..', 'public', 'index.html');
+    fs.readFile(filePath, (error, file) => {
+        if(error) {
+            console.log(error);
+            response.writeHead(500, {'Content-Type': 'text/html'});
+            response.end("<h1>Sorry, a dinosaur ate our server :(</h1>");
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(file);
+        }
+    });
+}
+
+module.exports = handleHome;
