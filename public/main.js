5d4526afe202b1aed1d0b1b20c0e0601dc0ea781
Jamie Carter <jc2820@hotmail.com>
2019-11-21 08:00:45 +0000

    extra notes

--- public/main.js
+++ public/main.js
@@ -5,3 +5,6 @@
 
 
 // window.onload = api();
+
+//once the search bar has a full country inside it, we can use the submit button to get the 
+// the relevant value from src/country-dish and use DOM to do further functionality.
