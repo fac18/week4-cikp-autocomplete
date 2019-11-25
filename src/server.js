8d1194800e9c82b76e87cf25718cb3f39c824d9f
Reda <redahaq@gmail.com>
2019-11-25 19:48:44 +0000

    deploy to heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const router = require('./router.js');
-
+const port = process.env.PORT || 3000;
 const server = http.createServer(router);
 
-server.listen(3000, () => console.log("Server listening on port 3000. Ready to accept requests :)"));
+server.listen(port, () => console.log("Server listening on port 3000. Ready to accept requests :)"));
